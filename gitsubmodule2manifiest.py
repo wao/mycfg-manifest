93113428941e58e24daba9dd8f8c10c0c4fc0c33
Chen Yang <yangchen@thinkmore.info>
2024-08-30 13:41:49 -0400

    implmenet gitsubmodule to manifest tools

---
+++ gitsubmodule2manifiest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+
+lines = Path("gm").read_text().strip().split("\n")
+
+while lines:
+    (s, p, u, *lines) = lines
+    path = p.split("=")[1].strip()
+    url = u.split("=")[1].strip()
+    if url.startswith("git@github.com:"):
+        url = url[len("git@github.com:"):]
+    elif url.startswith( "https://github.com/"):
+        url = url[len("https://github.com/"):]
+    else:
+        print("unknown url " + url)
+        exit(-1)
+
+    print(f'<project remote="github" name="{url}" path="{path}" remote="github" revision="master" />')
